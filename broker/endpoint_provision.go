5720acef4608541297b8538acb014e28dbdea545
Dr Nic Williams <drnicwilliams@gmail.com>
2017-02-21 14:55:39 +1000

    add provision endpoint which creates etcd role + grants access

--- broker/endpoint_provision.go
+++ broker/endpoint_provision.go
@@ -2,15 +2,45 @@ package broker
 
 import (
 	"context"
+	"fmt"
 
 	etcdclient "github.com/coreos/etcd/client"
+	"github.com/hashicorp/errwrap"
 	"github.com/pivotal-cf/brokerapi"
 )
 
 // Provision a new service instance
+// 1. Create role
+// 2. Grant path access to role
+// If they all already existed, then provisioning fails
+//
+// Binding will:
+// 3. Create user
+// 4. Assign role to user
 func (bkr *Broker) Provision(ctx context.Context, instanceID string, details brokerapi.ProvisionDetails, asyncAllowed bool) (resp brokerapi.ProvisionedServiceSpec, err error) {
-	authRoleAPI := etcdclient.NewAuthRoleAPI(bkr.EtcdClient)
+	roleName := bkr.serviceInstanceRole(instanceID)
+	rolePaths := bkr.serviceInstancePaths(instanceID)
 
-	authRoleAPI.ListRoles(ctx)
+	authRoleAPI := etcdclient.NewAuthRoleAPI(bkr.EtcdClient)
+	err = authRoleAPI.AddRole(ctx, roleName)
+	if err != nil {
+		err = errwrap.Wrapf("Could not add role: {{err}}", err)
+		return
+	}
+
+	grantedRole, err := authRoleAPI.GrantRoleKV(ctx, roleName, rolePaths, etcdclient.ReadWritePermission)
+	if err != nil {
+		err = errwrap.Wrapf("Could not grant role: {{err}}", err)
+		return
+	}
+	fmt.Printf("Created role %v\n", grantedRole)
 	return brokerapi.ProvisionedServiceSpec{}, nil
+}
+
+func (bkr *Broker) serviceInstanceRole(instanceID string) string {
+	return fmt.Sprintf("instance_%s", instanceID)
+}
+
+func (bkr *Broker) serviceInstancePaths(instanceID string) []string {
+	return []string{fmt.Sprintf("/service_instances/%s/*", instanceID)}
 }

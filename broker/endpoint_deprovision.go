2afec4bdaea2527a066aaf5f8f47a896e1ae144d
Dr Nic Williams <drnicwilliams@gmail.com>
2017-02-21 17:16:55 +1000

    deprovision deletes any data and the auth role

---
+++ broker/endpoint_deprovision.go
@@ -0,0 +1,35 @@
+package broker
+
+import (
+	"context"
+	"fmt"
+
+	etcdclient "github.com/coreos/etcd/client"
+	"github.com/hashicorp/errwrap"
+	"github.com/pivotal-cf/brokerapi"
+)
+
+// Deprovision service instance
+func (bkr *Broker) Deprovision(ctx context.Context, instanceID string, details brokerapi.DeprovisionDetails, asyncAllowed bool) (resp brokerapi.DeprovisionServiceSpec, err error) {
+	roleName := bkr.serviceInstanceRole(instanceID)
+
+	authRoleAPI := etcdclient.NewAuthRoleAPI(bkr.EtcdClient)
+	err = authRoleAPI.RemoveRole(ctx, roleName)
+	if err != nil {
+		err = errwrap.Wrapf("Could not remove role: {{err}}", err)
+		return
+	}
+
+	keysAPI := etcdclient.NewKeysAPI(bkr.EtcdClient)
+	deleteOpts := &etcdclient.DeleteOptions{
+		Recursive: true,
+		Dir:       true,
+	}
+	deletePath := bkr.serviceInstancePath(instanceID)
+	_, err = keysAPI.Delete(ctx, deletePath, deleteOpts)
+	if err != nil {
+		err = errwrap.Wrapf(fmt.Sprintf("Could not recursive delete '%s': {{err}}", deletePath), err)
+		return
+	}
+	return brokerapi.DeprovisionServiceSpec{}, nil
+}
--- broker/unimplemented.go
+++ broker/unimplemented.go
@@ -6,11 +6,6 @@ import (
 	"github.com/pivotal-cf/brokerapi"
 )
 
-// Deprovision service instance
-func (bkr *Broker) Deprovision(ctx context.Context, instanceID string, details brokerapi.DeprovisionDetails, asyncAllowed bool) (resp brokerapi.DeprovisionServiceSpec, err error) {
-	return brokerapi.DeprovisionServiceSpec{}, nil
-}
-
 // Update service instance
 func (bkr *Broker) Update(ctx context.Context, instanceID string, updateDetails brokerapi.UpdateDetails, asyncAllowed bool) (resp brokerapi.UpdateServiceSpec, err error) {
 	return brokerapi.UpdateServiceSpec{}, nil
